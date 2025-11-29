aee43ee6101efa2e955a209f9a8778e5d71374a9
Librellium <246878136+librellium@users.noreply.github.com>
2025-11-29 21:00:35 +0500

    Update builder

--- simpleforward/bot/builder.py
+++ simpleforward/bot/builder.py
@@ -1,6 +1,9 @@
+from typing import Optional
+
 from aiogram import Router
 
 from simpleforward.config import Config
+from simpleforward.moderation import AsyncModerator
 
 from .message_manager import MessageManager
 from .routers.photo import PhotoRouter
@@ -8,16 +11,18 @@ from .routers.start import StartRouter
 from .routers.text import TextRouter
 
 
-def build(config: Config, message_manager: MessageManager):
+def build(config: Config,
+          message_manager: MessageManager,
+          moderator: Optional[AsyncModerator] = None):
     main_router = Router()
 
     main_router.include_routers(
         StartRouter(),
         TextRouter(
-            config, message_manager
+            config, message_manager, moderator
         ),
         PhotoRouter(
-            config, message_manager
+            config, message_manager, moderator
         )
     )
 
