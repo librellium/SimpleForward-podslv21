1c1893306796e969d6153a7e1b0d0a0b916c99f7
Librellium <246878136+librellium@users.noreply.github.com>
2025-12-23 22:49:35 +0500

    Replace TemplateRenderer with Translator

--- anonflow/bot/builder.py
+++ anonflow/bot/builder.py
@@ -4,34 +4,35 @@ from aiogram import Router
 
 from anonflow.config import Config
 from anonflow.moderation import ModerationExecutor
+from anonflow.translator import Translator
 
 from .routers import InfoRouter, MediaRouter, StartRouter, TextRouter
-from .utils import EventHandler, MessageManager, TemplateRenderer
+from .utils import EventHandler, MessageManager
 
 
 def build(
     config: Config,
     message_manager: MessageManager,
-    template_renderer: TemplateRenderer,
+    translator: Translator,
     executor: Optional[ModerationExecutor] = None,
     event_handler: Optional[EventHandler] = None
 ):
     main_router = Router()
 
     main_router.include_routers(
-        StartRouter(template_renderer=template_renderer),
-        InfoRouter(template_renderer=template_renderer),
+        StartRouter(translator=translator),
+        InfoRouter(translator=translator),
         TextRouter(
             config=config,
             message_manager=message_manager,
-            template_renderer=template_renderer,
+            translator=translator,
             moderation_executor=executor,
             event_handler=event_handler,
         ),
         MediaRouter(
             config=config,
             message_manager=message_manager,
-            template_renderer=template_renderer,
+            translator=translator,
             moderation_executor=executor,
             event_handler=event_handler,
         ),
